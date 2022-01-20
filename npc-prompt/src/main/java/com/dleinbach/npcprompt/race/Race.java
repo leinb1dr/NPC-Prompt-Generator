962665eb4e3d4d070d04373097b1ccb06ab06451
Dan Leinbach <leinb1dr@gmail.com>
2022-01-20 04:23:04 -0500

    Fixed intellij autogenerated get/set/hash/string methods.

--- npc-prompt/src/main/java/com/dleinbach/npcprompt/race/Race.java
+++ npc-prompt/src/main/java/com/dleinbach/npcprompt/race/Race.java
@@ -2,11 +2,8 @@ package com.dleinbach.npcprompt.race;
 
 import com.dleinbach.npcprompt.utility.DiceParser;
 import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Id;
-
-import java.util.Objects;
 
 // Dragonborn x,9d20+160,3d27,medium
 public record Race(@Id Long id, String ageFormula, String weightFormula, String heightFormula, String characterSize) {
@@ -39,52 +36,4 @@ public record Race(@Id Long id, String ageFormula, String weightFormula, String 
     public int getHeight(){
             return DiceParser.parse(heightFormula).calc();
     }
-
-    @Id
-    public Long id() {
-        return id;
-    }
-
-    public String ageFormula() {
-        return ageFormula;
-    }
-
-    public String weightFormula() {
-        return weightFormula;
-    }
-
-    public String heightFormula() {
-        return heightFormula;
-    }
-
-    public String characterSize() {
-        return characterSize;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (Race) obj;
-        return Objects.equals(this.id, that.id) &&
-                Objects.equals(this.ageFormula, that.ageFormula) &&
-                Objects.equals(this.weightFormula, that.weightFormula) &&
-                Objects.equals(this.characterSize, that.characterSize);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, ageFormula, weightFormula, characterSize);
-    }
-
-    @Override
-    public String toString() {
-        return "Race[" +
-                "id=" + id + ", " +
-                "ageFormula=" + ageFormula + ", " +
-                "weightFormula=" + weightFormula + ", " +
-                "characterSize=" + characterSize + ']';
-    }
-
-
 }
